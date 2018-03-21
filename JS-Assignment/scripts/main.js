1f6c676fee8135ba5b4cb3e4494a62c80417e0da
tgutecht <35664243+tgutecht@users.noreply.github.com>
2018-03-21 00:22:13 -0500

    part 1 to 4 (extra & super challenges are missing)

--- JS-Assignment/scripts/main.js
+++ JS-Assignment/scripts/main.js
@@ -6,9 +6,21 @@ function update() {
 
     ///Part 1
     var firstName = getValue('firstName');
-    var lastName = getValue('lastName');
-    console.log(firstName);
-    console.log(lastName);
+ var lastName = getValue('lastName');
+ console.log(firstName);
+ console.log(lastName);
+document.getElementById("p1_fullName").innerHTML = firstName + " " + lastName;
+
+var p1_valid2
+if (firstName.length >0  && lastName.length >0)
+ {
+     p1_valid2 = true;
+ } else {
+     p1_valid2 = false;
+ }
+document.getElementById("p1_valid").innerHTML = p1_valid2;
+
+document.getElementById("p1_fullNameLength").innerHTML = firstName.length + lastName.length;
 
 
 
@@ -16,18 +28,42 @@ function update() {
     // Part 2
     var emailAddress = getValue('emailAddress');
     console.log(emailAddress);
-
+    document.getElementById('p2_email').innerHTML = emailAddress;
+    var emailAddressCheck;
+    if (emailAddress.charAt(0)> -1)
+      {
+        emailAddressCheck = true;
+      } else {
+        emailAddressCheck = false;
+      }
+    document.getElementById("p2_valid").innerHTML = emailAddressCheck;
+      document.getElementById("p2_emailLength").innerHTML = emailAddress.length;
 
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
-
+    document.getElementById("p3_number").innerHTML = randomNumber;
+    var x ;
+   if (isNaN(randomNumber))
+   {x = false; } else
+   {
+     x = true;
+   };
+   document.getElementById('p3_valid').innerHTML = x;
 
 
 
     // Part 4
     var arrayInput = getValue('arrayValue');
     console.log(arrayInput);
+    /*Add the value of `arrayInput` to the array named `arrayItems`. `arrayItems is declared on the first line of the file.
+    - `p4_arraylength` - the number of items in `arrayItems`
+    - `p4_valid` - the value of `arrayItems`*/
+    arrayItems.push(arrayInput);
+    document.getElementById('p4_arraylength').innerHTML = arrayItems.push("arrayInput");
+    document.getElementById('p4_valid').innerHTML = arrayItems;
+
+    
 
 
 
@@ -55,4 +91,3 @@ function getValue(id) {
 function getElm(id) {
     return document.getElementById(id);
 }
-
